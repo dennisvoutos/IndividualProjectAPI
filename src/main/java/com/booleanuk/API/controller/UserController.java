c47eee7b4932ef6351a4f8d9ef3534816294e1b5
Dennis <dennisvoutos@gmail.com>
2023-06-13 17:30:06 +0300

    basics of API done

--- src/main/java/com/booleanuk/API/controller/UserController.java
+++ src/main/java/com/booleanuk/API/controller/UserController.java
@@ -4,10 +4,7 @@ import com.booleanuk.API.model.User;
 import com.booleanuk.API.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
@@ -26,5 +23,29 @@ public class UserController {
     public User getUserById(@PathVariable int id){
         return this.userRepository.findById(id)
                 .orElseThrow(()->new ResponseStatusException(HttpStatus.NOT_FOUND));
+    }
+    @PostMapping
+    public User createUser(@RequestBody User user){
+        if(user.getEmail() == null || user.getPassword() == null || user.getUsername() == null){
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
+        }
+        return this.userRepository.save(user);
+    }
+    @DeleteMapping("/{id}")
+    public User deleteUser(@PathVariable int id){
+        User deleted = this.getUserById(id);
+        this.userRepository.delete(deleted);
+        return deleted;
+    }
+    @PutMapping("/{id}")
+    public User updateUser(@PathVariable int id, @RequestBody User user){
+        User updated = this.getUserById(id);
+        if(user.getEmail() == null || user.getPassword() == null || user.getUsername() == null){
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
+        }
+        updated.setEmail(user.getEmail());
+        updated.setPassword(user.getPassword());
+        updated.setUsername(user.getUsername());
+        return this.userRepository.save(updated);
     }
 }
